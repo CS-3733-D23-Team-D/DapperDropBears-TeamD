e4a8c4f3e13ed5cd2d47e63ce367f60a0d4f8389
jtkardon <jtkardon@gmail.com>
2023-03-25 14:54:45 -0400

    Made Edge Class

--- src/main/java/edu/wpi/teamname/Edge.java
+++ src/main/java/edu/wpi/teamname/Edge.java
@@ -1,4 +1,17 @@
 package edu.wpi.teamname;
+import lombok.Getter;
+import lombok.Setter;
 
 public class Edge {
+    @Getter @Setter String edgeID;
+    @Getter @Setter String startNode;
+    @Getter @Setter String endNode;
+
+    // Returns all the attributes of a Node as a String
+    public String toString() {
+        String eIDs = "EdgeID: " + edgeID;
+        String startNodeS = "Starting Node: " + startNode;
+        String endNodeS = "Ending Node: " + endNode;
+        return eIDs + " " + startNodeS + " " + endNodeS;
+    }
 }
