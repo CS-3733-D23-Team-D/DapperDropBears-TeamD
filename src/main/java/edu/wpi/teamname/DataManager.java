6f63ad48ea4c4e6790ec5aea4ab78668078ef5f5
Addy <abmartin@wpi.edu>
2023-03-26 18:07:06 -0400

    better excel import

--- src/main/java/edu/wpi/teamname/DataManager.java
+++ src/main/java/edu/wpi/teamname/DataManager.java
@@ -3,119 +3,85 @@ package edu.wpi.teamname;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class DataManager {
 
   private static final String DB_URL = "jdbc:postgresql://database.cs.wpi.edu:5432/teamddb";
-  private static final String DB_USER = "teamddb";
+  private static final String DB_USER = "teamd";
   private static final String DB_PASSWORD = "teamd40";
-  private static int numColumns;
-
-  public DataManager(String filePath, String tableName) {}
 
   /** Prototype 1: method should read file from CSV and import data to teamd database */
-  public static void importCSV(String csvFilePath, String tableName) {
-
-    try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
-      String insertQuery = generateInsertQuery(tableName);
-
-      try (PreparedStatement ps = conn.prepareStatement(insertQuery);
-          BufferedReader br = new BufferedReader(new FileReader(csvFilePath))) {
-
-        // Read the CSV file line by line
-        String line;
-        while ((line = br.readLine()) != null) {
-          // Split each line by comma separator
-          String[] values = line.split(",");
-
-          // Set the values of the PreparedStatement
-          for (int i = 1; i <= values.length; i++) {
-            ps.setString(i, values[i - 1]);
-          }
-
-          // Execute the PreparedStatement
-          ps.executeUpdate();
-        }
-
-        System.out.println("CSV import completed successfully.");
-
-      } catch (SQLException e) {
-        e.printStackTrace();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
-  }
-
-  private static String generateInsertQuery(String tableName) {
-    String query = "INSERT INTO " + tableName + " VALUES(";
-    // Assume the CSV file has the same number of columns as the database table
-    for (int i = 1; i <= numColumns; i++) {
-      query += "?";
-      if (i != numColumns) {
-        query += ",";
-      }
-    }
-    query += ")";
-    return query;
-  }
-  // Import CSV data to PostgreSQL table
-  public static void importToPostgreSQL(
-      String csvFilePath, String dbURL, String dbUsername, String dbPassword, String tableName) {
-
-    try {
-      // Load PostgreSQL JDBC driver
-      Class.forName("org.postgresql.Driver");
-
-      // Open database connection
-      Connection conn = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
-
-      // Prepare SQL statement to insert data into table
-      String insertSQL = "INSERT INTO " + tableName + " VALUES (?, ?, ?, ?, ?)";
-      PreparedStatement ps = conn.prepareStatement(insertSQL);
-
-      // Read CSV file and insert data into table
-      BufferedReader br = new BufferedReader(new FileReader(csvFilePath));
+  public static List<String[]> importCSV(String fileName) {
+    System.out.println("Parsing CSV file: " + fileName);
+    List<String[]> rows = new ArrayList<>();
+    try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
       String line;
-      while ((line = br.readLine()) != null) {
-        String[] values = line.split(",");
-        ps.setInt(1, Integer.parseInt(values[0]));
-        ps.setString(2, values[1]);
-        ps.setString(3, values[2]);
-        ps.setString(4, values[3]);
-        ps.setDouble(5, Double.parseDouble(values[4]));
-        ps.executeUpdate();
+      while ((line = reader.readLine()) != null) {
+        String[] row = line.split(",", -1);
+        rows.add(row);
       }
-
-      // Close resources
-      ps.close();
-      br.close();
-      conn.close();
-
-      System.out.println("CSV data imported to PostgreSQL table successfully!");
-
-    } catch (Exception e) {
-      e.printStackTrace();
+      System.out.println("CSV file parsed successfully");
+      return rows;
+    } catch (IOException e) {
+      System.err.println("Error parsing CSV file: " + e.getMessage());
+      return null;
     }
   }
 
-  public static void main(String[] args) {
+  //  public static void uploadToPostgreSQL(List<String[]> csvData) throws SQLException {
+  //    String url =
+  //        "jdbc:postgresql://database.cs.wpi.edu:5432/teamddb"; // replace with your database URL
+  //    String user = "teamd"; // replace with your database username
+  //    String password = "teamd40"; // replace with your database password
+  //
+  //    try (Connection conn = DriverManager.getConnection(url, user, password)) {
+  //      String query =
+  //          "INSERT INTO mytable (nodeID, xcoord, ycoord, floor, building, nodeType, longName,
+  // shortName) "
+  //              + "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+  //      PreparedStatement statement = conn.prepareStatement(query);
+  //
+  //      for (String[] row : csvData) {
+  //        statement.setInt(1, Integer.parseInt(row[0])); // assuming nodeID is an integer column
+  //        statement.setDouble(2, Double.parseDouble(row[1])); // assuming xcoord is a double
+  // column
+  //        statement.setDouble(3, Double.parseDouble(row[2])); // assuming ycoord is a double
+  // column
+  //        statement.setString(4, row[3]); // assuming floor is a string column
+  //        statement.setString(5, row[4]); // assuming building is a string column
+  //        statement.setString(6, row[5]); // assuming nodeType is a string column
+  //        statement.setString(7, row[6]); // assuming longName is a string column
+  //        statement.setString(8, row[7]); // assuming shortName is a string column
+  //
+  //        statement.executeUpdate();
+  //      }
+  //      System.out.println("CSV data uploaded to PostgreSQL database");
+  //    } catch (SQLException e) {
+  //      System.err.println("Error uploading CSV data to PostgreSQL database: " + e.getMessage());
+  //    }
+  //  }
+
+  public static void main(String[] args) throws SQLException {
     Scanner scanner = new Scanner(System.in);
-
+    // No quotes when importing doc
     System.out.print("Enter the file path of the CSV file to import: ");
-    String filePath = scanner.nextLine();
-
-    System.out.print("Enter the name of the table in Postgres to export to: ");
-    String tableName = scanner.nextLine();
-
-    DataManager readCSV = new DataManager(filePath, tableName);
-    readCSV.importToPostgreSQL(filePath, tableName, DB_URL, DB_USER, DB_PASSWORD);
+    String csvFileName = scanner.nextLine();
+    List<String[]> rows = importCSV(csvFileName);
+    if (rows != null) {
+      System.out.println("Number of rows: " + rows.size());
+      for (String[] row : rows) {
+        System.out.println(Arrays.toString(row));
+      }
+      //      try {
+      //        uploadToPostgreSQL(rows);
+      //      } catch (SQLException e) {
+      //        System.err.println(e.getMessage());
+      //      }
+    }
   }
 }
