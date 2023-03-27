6d0d67397f7ebf650c3ae0207ab48169ff83ea3d
ajbriantiwpi <ajbrianti@wpi.edu>
2023-03-26 21:34:28 -0400

    Created home page, added signage page, added some small buttons

--- src/main/java/edu/wpi/teamname/controllers/HomeController.java
+++ src/main/java/edu/wpi/teamname/controllers/HomeController.java
@@ -6,11 +6,15 @@ import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
 
 public class HomeController {
-// test push
-  @FXML MFXButton navigateButton;
+  // test push
+  @FXML MFXButton serviceRequests;
+  @FXML MFXButton helpButton;
+  @FXML MFXButton directionButton;
+  @FXML MFXButton exitButton;
 
   @FXML
   public void initialize() {
-    navigateButton.setOnMouseClicked(event -> Navigation.navigate(Screen.SERVICE_REQUEST));
+    serviceRequests.setOnMouseClicked(event -> Navigation.navigate(Screen.SERVICE_REQUEST));
+    exitButton.setOnMouseClicked(event -> Navigation.navigate(Screen.SIGNAGE));
   }
 }
