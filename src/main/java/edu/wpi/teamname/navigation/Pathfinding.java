12355df34357d02d06b7d2778f8528b8fcd7244d
arturolemos <arturo09lemos@gmail.com>
2023-03-29 21:28:14 -0400

    Nodes and Edges object initialization

--- src/main/java/edu/wpi/teamname/navigation/Pathfinding.java
+++ src/main/java/edu/wpi/teamname/navigation/Pathfinding.java
@@ -5,82 +5,60 @@ import java.util.*;
 import java.util.Scanner;
 
 public class Pathfinding {
-  private int[][] graph; // hardcoded graph
-  private boolean[] visited; // visited nodes
-  private int[] parent; // integer array to keep track of parent of each node
+  private static Node[] Nodes = new Node[46];
+  private static Edge[] Edges = new Edge[46];
+  private static String NodesFile =
+      "C:\\Users\\artur\\IdeaProjects\\DapperDropBears-TeamD\\src\\main\\java\\edu\\wpi\\teamname\\navigation\\L1Nodes.csv";
+  private static String EdgesFile =
+      "C:\\Users\\artur\\IdeaProjects\\DapperDropBears-TeamD\\src\\main\\java\\edu\\wpi\\teamname\\navigation\\L1Edges.csv";
 
-  // constructor
-  public Pathfinding(int[][] graph) {
-    this.graph = graph;
-    visited = new boolean[graph.length];
-    parent = new int[graph.length];
-    Arrays.fill(parent, -1); // initialize parent array with -1
-  }
-
-  // method to get the shortest path between 2 points
-  public List<Integer> bfsBacktrack(int start, int dest) {
-    List<Integer> path = new ArrayList<>();
-    Queue<Integer> queue = new LinkedList<>();
-    visited[start] = true;
-    queue.add(start);
-
-    while (!queue.isEmpty()) {
-      int curr = queue.poll();
-      if (curr == dest) { // destination found
-        // backtrack to start and add nodes to path
-        int node = dest;
-        while (node != start) {
-          path.add(node);
-          System.out.println("This is parent node:" + parent[node]);
-          node = parent[node];
-        }
-        path.add(start);
-        Collections.reverse(path);
-        return path;
-      }
-      for (int i = 0; i < graph.length; i++) {
-        if (graph[curr][i] == 1 && !visited[i]) { // if node is adjacent and not visited
-          visited[i] = true;
-          parent[i] = curr;
-          queue.add(i);
-        }
-      }
-    }
-    return null; // destination not reachable from start
-  }
-
-  // main method to test the algorithm
   public static void main(String[] args) throws Exception {
-    //    Node A = new Node("CCONF001L1",	2255,	849,	"L1",	"45 Francis",	"CONF",	"Anesthesia Conf
-    // Floor L1",	"Conf");
-    //    Node B = new Node();
-    //    Node C = new Node();
-    //    Node D = new Node();
-    //    Node E = new Node();
 
-    // Edge A =
-    String file =
-        "C:\\Users\\artur\\IdeaProjects\\DapperDropBears-TeamD\\src\\main\\java\\edu\\wpi\\teamname\\navigation\\L1Nodes.csv";
-    Scanner sc = new Scanner(new File(file));
+    ArrayList<String> NodeLines = ReadCsvLines(NodesFile);
+    ArrayList<String> EdgeLines = ReadCsvLines(EdgesFile);
 
-    int i = 1;
-    ArrayList<String> NodeLine = new ArrayList<String>();
+    initializeNodes(NodeLines);
+    initializeEdges(EdgeLines);
+    System.out.println(Nodes[0].getXCord());
+    System.out.println(Edges[0].getEdgeID());
+  }
+
+  public static ArrayList<String> ReadCsvLines(String filename) throws Exception {
+    // Create a csv scanner object
+    Scanner sc = new Scanner(new File(filename));
+
+    // Pull the csv file lines
+    ArrayList<String> lines = new ArrayList<String>();
     while (sc.hasNextLine()) {
-      NodeLine.add(sc.nextLine());
+      lines.add(sc.nextLine());
+    }
+
+    // Remove the tittles
+    lines.remove(0);
+    return lines;
+  }
+
+  public static void initializeNodes(ArrayList<String> NodeLines) {
+    // Initialize the nodes with the node lines data
+    int i = 0;
+    while (!NodeLines.isEmpty()) {
+      String[] I = NodeLines.get(0).split(",");
+      NodeLines.remove(0);
+      Nodes[i] =
+          new Node(
+              I[0], Integer.parseInt(I[1]), Integer.parseInt(I[2]), I[3], I[4], I[5], I[6], I[7]);
       i++;
     }
+  }
 
-    String[] I;
-    int j = 0;
-    String line = "";
-    while (!NodeLine.isEmpty()) {
-      line = NodeLine.get(j);
-      I = line.split(",");
-      NodeLine.remove(j);
-
-      Node node[](I[0], Integer.parseInt(I[1]), Integer.parseInt(I[2]), I[3], I[4], I[5], I[6], I[7]);
+  public static void initializeEdges(ArrayList<String> EdgeLines) {
+    // Initialize the nodes with the node lines data
+    int i = 0;
+    while (!EdgeLines.isEmpty()) {
+      String[] E = EdgeLines.get(0).split(",");
+      EdgeLines.remove(0);
+      Edges[i] = new Edge(E[0], E[1], E[2]);
+      i++;
     }
-
-    sc.close();
   }
 }
