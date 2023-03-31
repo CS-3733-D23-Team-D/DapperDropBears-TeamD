62b604617b8b4a634be17cba2e81fef2c78fa5f5
geniusturtle3 <spmarkwick@wpi.edu>
2023-03-31 00:48:20 -0400

    custom button testing with some other junk since this repo wont be used anymore

--- src/main/java/edu/wpi/teamname/controllers/TestController.java
+++ src/main/java/edu/wpi/teamname/controllers/TestController.java
@@ -1,10 +1,28 @@
 package edu.wpi.teamname.controllers;
 
+import edu.wpi.teamname.servicerequests.JFXItems.MenuItem;
+import java.util.ArrayList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
 public class TestController {
+  @FXML VBox vBox;
+
+  ArrayList<String> testList = new ArrayList<>();
+  @FXML ObservableList<MenuItem> itemlist = FXCollections.observableArrayList();
 
   public void initialize() {
     //    FXMLLoader will now automatically call any suitably annotated no-arg initialize() (With
     // .load())
     System.out.println("TestININT!");
+    testList.add("1");
+    testList.add("2");
+
+    for (String item : testList) {
+      vBox.getChildren().add(new SubObjects(item));
+      // vBox.getChildren().
+    }
   }
 }
--- src/main/java/edu/wpi/teamname/requestItems/MealItem.java
+++ src/main/java/edu/wpi/teamname/requestItems/MealItem.java
@@ -1,7 +1,7 @@
 package edu.wpi.teamname.requestItems;
 
 public class MealItem extends RequestItem {
-  public MealItem(String name) {
-    super(name);
+  public MealItem(String name, double price) {
+    super(name, price);
   }
 }
