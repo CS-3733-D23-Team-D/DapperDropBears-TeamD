f30afc1cc85e90c1e76f580bfd113b2462687994
ajbriantiwpi <ajbrianti@wpi.edu>
2023-04-01 16:39:36 -0400

    oops

---
+++ src/main/java/edu/wpi/teamname/controllers/LoginController.java
@@ -0,0 +1,12 @@
+package edu.wpi.teamname.controllers;
+
+
+import edu.wpi.teamname.navigation.Navigation;
+import edu.wpi.teamname.navigation.Screen;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginController {
+}
