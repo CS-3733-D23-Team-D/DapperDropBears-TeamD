071aa81eeaafa60b0b5fd83cb33c88d28f7d5f75
ajbriantiwpi <ajbrianti@wpi.edu>
2023-04-02 22:16:03 -0400

    added the forgot password button and bound the button to the text

--- src/main/java/edu/wpi/teamname/controllers/LoginController.java
+++ src/main/java/edu/wpi/teamname/controllers/LoginController.java
@@ -1,22 +1,25 @@
 package edu.wpi.teamname.controllers;
 
 import io.github.palexdev.materialfx.controls.MFXButton;
+import java.awt.*;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class LoginController {
   @FXML MFXButton exit;
 
-  @FXML
-  private void username() {}
-
-  @FXML
-  private void password() {}
-
-  @FXML
-  void updatePassword() {}
+  @FXML MFXButton loginButton;
+  @FXML MFXButton forgotPassword;
+  @FXML TextField loginText;
+  @FXML PasswordField passwordText;
 
   @FXML
   public void initialize() {
     exit.setOnMouseClicked(event -> System.exit(0));
+    loginButton.disableProperty().bind(Bindings.isEmpty(loginText.textProperty()));
+    loginButton.disableProperty().bind((Bindings.isEmpty(passwordText.textProperty())));
   }
+
 }
