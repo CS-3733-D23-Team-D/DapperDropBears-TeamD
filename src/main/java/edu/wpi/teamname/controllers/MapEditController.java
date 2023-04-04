3e523f5dd41a2d3079317d774c44602a950d9822
C-Slat <ctslattery@wpi.edu>
2023-04-03 21:36:52 -0400

    Added node editing table

---
+++ src/main/java/edu/wpi/teamname/controllers/MapEditController.java
@@ -0,0 +1,19 @@
+package edu.wpi.teamname.controllers;
+
+import edu.wpi.teamname.navigation.Navigation;
+import edu.wpi.teamname.navigation.Screen;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.fxml.FXML;
+
+public class MapEditController {
+
+    @FXML
+    MFXButton backButton;
+
+    @FXML
+    public void initialize() {
+        backButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
+    }
+}
+
+
