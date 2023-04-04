917c9508e8393a2a8c12f4e4b64aa8e8c7d2f49c
C-Slat <ctslattery@wpi.edu>
2023-04-03 21:46:09 -0400

    back button attempt

--- src/main/java/edu/wpi/teamname/controllers/MapEditController.java
+++ src/main/java/edu/wpi/teamname/controllers/MapEditController.java
@@ -5,15 +5,15 @@ import edu.wpi.teamname.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
 
-public class MapEditController {
+//public class MapEditController {
 
-    @FXML
-    MFXButton backButton;
+    //@FXML
+    //MFXButton backButton;
 
-    @FXML
-    public void initialize() {
-        backButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
-    }
-}
+    //@FXML
+  //  public void initialize() {
+   //     backButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
+    //}
+//}
 
 
