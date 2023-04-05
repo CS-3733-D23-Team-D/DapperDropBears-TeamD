0628d2de9b0753f94fb3f938f2ccf0a95cd22281
asamarin22 <asamarin@wpi.edu>
2023-04-05 12:31:47 -0400

    changed Initialization Node/Edges & Error handling

--- src/main/java/edu/wpi/teamname/navigation/Pathfinding.java
+++ src/main/java/edu/wpi/teamname/navigation/Pathfinding.java
@@ -7,39 +7,40 @@ import java.util.Scanner;
 public class Pathfinding {
   private static List<Node> Nodes = new ArrayList<Node>();
   private static List<Edge> Edges = new ArrayList<Edge>();
-  private static String NodesFile = "C:\\Users\\Aleksandr Samarin\\Downloads\\Node.csv";
-  private static String EdgesFile = "C:\\Users\\Aleksandr Samarin\\Downloads\\Edge.csv";
-
   private static ArrayList<String> linesOfNodeInfo = new ArrayList<String>();
   private static ArrayList<String> linesOfEdgeInfo = new ArrayList<String>();
 
   public static void main(String[] args) throws Exception {
-    // initializeNodes(ReadCsvLines(NodesFile));
-    // initializeEdges(ReadCsvLines(EdgesFile));
     ArrayList<Node> listOfNodes = Node.getAllNodes();
     ArrayList<Edge> listOfEdges = Edge.getAllEdges();
 
-    //System.out.println(nodeToString(listOfNodes.get(0)));
+    //    for (int i = 0; i < listOfNodes.size(); i++) {
+    //      linesOfNodeInfo.add(nodeToString(listOfNodes.get(i)));
+    //    }
 
-    for (int i = 0; i < listOfNodes.size(); i++) {
-      linesOfNodeInfo.add(nodeToString(listOfNodes.get(i)));
-    }
+    //    for (int i = 0; i < listOfEdges.size(); i++) {
+    //      linesOfEdgeInfo.add(edgeToString(listOfEdges.get(i)));
+    //    }
 
-    for (int i = 0; i < listOfEdges.size(); i++) {
-      linesOfEdgeInfo.add(edgeToString(listOfEdges.get(i)));
-    }
-
-    initializeNodes(linesOfNodeInfo);
-    initializeEdges(linesOfEdgeInfo);
-
-    //System.out.println(Nodes.get(580).getId());
+    initializeNodes(listOfNodes);
+    initializeEdges(listOfEdges);
 
     Scanner sc = new Scanner(System.in); // System.in is a standard input stream
     System.out.print("Enter Start NodeID: ");
     int startNodeID = sc.nextInt();
 
+    if (startNodeID < 100 || startNodeID > 3000 || startNodeID % 5 != 0) {
+      System.out.println("Not an acceptable start node. Please run program to try again.");
+      System.exit(0);
+    }
+
     System.out.print("Enter End NodeID: ");
     int endNodeID = sc.nextInt();
+
+    if (endNodeID < 100 || endNodeID > 3000 || endNodeID % 5 != 0) {
+      System.out.println("Not an acceptable end node. Please run program to try again.");
+      System.exit(0);
+    }
 
     Graph graph = new Graph(Nodes, Edges);
 
@@ -50,21 +51,22 @@ public class Pathfinding {
     AStar.printPath(graph.getTarget());
   }
 
-  public static ArrayList<String> ReadCsvLines(String filename) throws Exception {
-    // Create a csv scanner object
-    Scanner sc = new Scanner(new File(filename));
+  //  public static ArrayList<String> ReadCsvLines(String filename) throws Exception {
+  //    // Create a csv scanner object
+  //    Scanner sc = new Scanner(new File(filename));
+  //
+  //    // Pull the csv file lines
+  //    ArrayList<String> lines = new ArrayList<String>();
+  //    while (sc.hasNextLine()) {
+  //      lines.add(sc.nextLine());
+  //    }
+  //
+  //    // Remove the title
+  //    lines.remove(0);
+  //    return lines;
+  //  }
 
-    // Pull the csv file lines
-    ArrayList<String> lines = new ArrayList<String>();
-    while (sc.hasNextLine()) {
-      lines.add(sc.nextLine());
-    }
-
-    // Remove the title
-    lines.remove(0);
-    return lines;
-  }
-
+  // Method to convert node data to string
   public static String nodeToString(Node n) {
     String nodeInfo = "";
     nodeInfo =
@@ -72,31 +74,39 @@ public class Pathfinding {
     return nodeInfo;
   }
 
+  // Method to convert edge data to string
   public static String edgeToString(Edge e) {
     String edgeInfo = "";
     edgeInfo = e.getStartNodeID() + "," + e.getEndNodeID();
     return edgeInfo;
   }
 
-  public static void initializeNodes(ArrayList<String> NodeLines) {
+  // Taking strings, parse the data coming from
+  public static void initializeNodes(ArrayList<Node> NodeLines) {
     // Initialize the nodes with the node lines data
     while (!NodeLines.isEmpty()) {
-      String[] I = NodeLines.get(0).split(",");
-      NodeLines.remove(0);
+      // String[] I = NodeLines.get(0).split(",");
+      // NodeLines.remove(0);
       Nodes.add(
           new Node(
-              Integer.parseInt(I[0]), Integer.parseInt(I[1]), Integer.parseInt(I[2]), I[3], I[4]));
+              NodeLines.get(0).getId(),
+              NodeLines.get(0).getX(),
+              NodeLines.get(0).getY(),
+              NodeLines.get(0).getFloor(),
+              NodeLines.get(0).getBuilding()));
+      NodeLines.remove(0);
     }
   }
 
-  public static void initializeEdges(ArrayList<String> EdgeLines) {
+  // Initializes the edges given the list of string containing edge info
+  public static void initializeEdges(ArrayList<Edge> EdgeLines) {
     // Initialize the nodes with the node lines data
     int i = 0;
     while (!EdgeLines.isEmpty()) {
-      String[] E = EdgeLines.get(0).split(",");
+      // String[] E = EdgeLines.get(0).split(",");
+      // EdgeLines.remove(0);
+      Edges.add(new Edge(EdgeLines.get(0).getStartNodeID(), EdgeLines.get(0).getEndNodeID()));
       EdgeLines.remove(0);
-      Edges.add(new Edge(Integer.parseInt(E[0]), Integer.parseInt(E[1])));
-      i++;
     }
   }
 }
