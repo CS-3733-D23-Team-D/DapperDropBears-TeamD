90e02b434713a8e1ebc0483ee14f21a2af34a19f
ajbriantiwpi <ajbrianti@wpi.edu>
2023-04-05 20:26:23 -0400

    adjusted the button to navigate to the view of requests

--- src/main/java/edu/wpi/teamname/navigation/Screen.java
+++ src/main/java/edu/wpi/teamname/navigation/Screen.java
@@ -13,7 +13,7 @@ public enum Screen {
 
   //  REQ_MENU("views/ServiceRequestMenu.fxml"),
   LOGIN("views/Login.fxml"),
-  SIGNAGE("views/SignageLevels.fxml"),
+  //  SIGNAGE("views/SignageLevels.fxml"),
   SERVICE_REQUEST_VIEW("views/ServiceRequestView.fxml");
   private final String filename;
 
